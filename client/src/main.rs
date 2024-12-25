c497fbe14f8fc76aefa96d1d59001f7cd8814371
sh3ll3x3c <ljubisa@ethernal.tech>
2024-12-25 16:56:36 +0100

    use client state for ping data

--- client/src/main.rs
+++ client/src/main.rs
@@ -38,6 +38,7 @@ use tokio::{
 	sync::{
 		broadcast,
 		mpsc::{self, UnboundedReceiver},
+		Mutex,
 	},
 };
 use tracing::{error, info, span, trace, warn, Level};
@@ -333,6 +334,7 @@ async fn run(
 			.await;
 	}));
 
+	tracking::run(Duration::from_secs(2), &identity_cfg.avail_key_pair, state);
 	Ok(())
 }
 
